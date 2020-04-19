4306d3267fc5cfa260f17f6d256274bcbfd1e44e
Matthew Castrigno <matthewcastrigno@u.boisestate.edu>
2020-04-19 16:02:40 -0600

    first local commit

---
+++ PayApp/PayApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PayApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
